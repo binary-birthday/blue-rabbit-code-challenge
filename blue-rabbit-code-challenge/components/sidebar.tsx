044bc41753970f5e3e2728db6344544ef54e0d47
Wade Martin <wademartin.dev@icloud.com>
2022-06-21 20:33:37 -0400

    minor fixes

--- blue-rabbit-code-challenge/components/sidebar.tsx
+++ blue-rabbit-code-challenge/components/sidebar.tsx
@@ -38,9 +38,6 @@ const Sidebar = () => {
       color="gray"
     >
       <Box paddingY="20px" height="100%">
-        <Box width="120px" marginBottom="20px" paddingX="20px">
-          <NextImage src="/vercel.svg" height={60} width={120} />
-        </Box>
         <Box marginBottom="20px">
           <List spacing={2}>
             {navMenu.map((menu) => (
