2fd825627703203b861e05051dd772f200162f42
Wade Martin <wademartin.dev@icloud.com>
2022-06-21 21:05:28 -0400

    update env variable names

--- pages/api/imageUpload.ts
+++ pages/api/imageUpload.ts
@@ -3,18 +3,18 @@ import aws from 'aws-sdk';
 export default async function handler(req, res) {
   try {
 	  const s3 = new aws.S3({
-	    accessKeyId: process.env.AWS_ACCESS_KEY,
-	    secretAccessKey: process.env.AWS_SECRET_KEY,
-	    region: process.env.AWS_REGION,
+	    accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+	    secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+	    region: process.env.APP_AWS_REGION,
 	  })
 	  aws.config.update({
-		  accessKeyId: process.env.AWS_ACCESS_KEY,
-	    secretAccessKey: process.env.AWS_SECRET_KEY,
-	    region: process.env.AWS_REGION,
+		  accessKeyId: process.env.APP_AWS_ACCESS_KEY,
+	    secretAccessKey: process.env.APP_AWS_SECRET_KEY,
+	    region: process.env.APP_AWS_REGION,
 	    signatureVersion: 'v4',
 	  })
 	  const post = await s3.createPresignedPost({
-	    Bucket: process.env.AWS_BUCKET_NAME,
+	    Bucket: process.env.APP_AWS_BUCKET_NAME,
 	    Fields: {
 	      key: req.query.file,
 	    },
